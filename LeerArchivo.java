4f1f00c99ac80c280a4180ae7d1d37a05eb6b028
stephaniefab <81348001+stephaniefab@users.noreply.github.com>
2022-08-06 17:10:44 -0600

    Add files via upload

---
+++ LeerArchivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.leerarchivo;
+
+/**
+ *
+ * @author fabio
+ */
+public class LeerArchivo {
+
+    public static void main(String[] args) {
+        formLeer ven= new formLeer();
+        ven.setVisible(true);
+    }
+}
